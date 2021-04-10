ee6b5b0904d2a666ec618f84ef8e8de503ef1ff7
ojacomarket <elfutbolesmivida@mail.ru>
2021-04-10 11:51:30 +0300

    Add boolean to check whos turn in a game

--- src/main/java/com/reji/tictactoe/tictactoe/TicTacToeGame.java
+++ src/main/java/com/reji/tictactoe/tictactoe/TicTacToeGame.java
@@ -12,6 +12,7 @@ public class TicTacToeGame implements ActionListener {
     private final JPanel BUTTONS_PANEL = new JPanel();
     private final JLabel TEXT_FIELD = new JLabel();
     private final List<JButton>  BUTTONS_ITSELF = new ArrayList<>();
+    private boolean isPlayerOneTurn = true;
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 

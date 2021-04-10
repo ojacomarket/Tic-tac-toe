d8f625e3b08848d671d9440d0eab635903a57aaa
ojacomarket <elfutbolesmivida@mail.ru>
2021-04-10 11:48:04 +0300

    Add JLabel to display messages for user

--- src/main/java/com/reji/tictactoe/tictactoe/TicTacToeGame.java
+++ src/main/java/com/reji/tictactoe/tictactoe/TicTacToeGame.java
@@ -8,6 +8,7 @@ public class TicTacToeGame implements ActionListener {
     private final JFrame GAME_FIELD = new JFrame();
     private final JPanel GAME_PANEL = new JPanel();
     private final JPanel BUTTONS = new JPanel();
+    private final JLabel TEXT_FIELD = new JLabel();
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 

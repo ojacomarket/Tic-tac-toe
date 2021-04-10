6c18dd230d8aeb832ceb30c558b8f543eab89e82
ojacomarket <elfutbolesmivida@mail.ru>
2021-04-10 11:49:46 +0300

    Add REAL buttons and fix JPanel for buttons name

--- src/main/java/com/reji/tictactoe/tictactoe/TicTacToeGame.java
+++ src/main/java/com/reji/tictactoe/tictactoe/TicTacToeGame.java
@@ -3,12 +3,15 @@ package com.reji.tictactoe.tictactoe;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TicTacToeGame implements ActionListener {
     private final JFrame GAME_FIELD = new JFrame();
     private final JPanel GAME_PANEL = new JPanel();
-    private final JPanel BUTTONS = new JPanel();
+    private final JPanel BUTTONS_PANEL = new JPanel();
     private final JLabel TEXT_FIELD = new JLabel();
+    private final List<JButton>  BUTTONS_ITSELF = new ArrayList<>();
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 

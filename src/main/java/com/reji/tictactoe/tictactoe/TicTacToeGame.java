bf2c13b4eecc1d5cd929115d0e6da54825e28885
ojacomarket <elfutbolesmivida@mail.ru>
2021-04-10 11:46:16 +0300

    Add JFrame entities

--- src/main/java/com/reji/tictactoe/tictactoe/TicTacToeGame.java
+++ src/main/java/com/reji/tictactoe/tictactoe/TicTacToeGame.java
@@ -1,9 +1,12 @@
 package com.reji.tictactoe.tictactoe;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class TicTacToeGame implements ActionListener {
+    private final JFrame GAME_FIELD = new JFrame();
+    private final JPanel GAME_PANEL = new JPanel();
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 

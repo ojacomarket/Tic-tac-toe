cc945be1ecb7d7071b3fdaf6c97a294ca2c01845
ojacomarket <elfutbolesmivida@mail.ru>
2021-04-10 14:14:24 +0300

    Add method to decide whos turn to play and call it inside constructor method

--- src/main/java/com/reji/tictactoe/tictactoe/TicTacToeGame.java
+++ src/main/java/com/reji/tictactoe/tictactoe/TicTacToeGame.java
@@ -20,21 +20,23 @@ public class TicTacToeGame implements ActionListener {
     private final Random generator = new Random();
     private boolean isPlayerOneTurn;
     private final Consumer<JButton> initButtons = BUTTONS_PANEL::add;
-    public TicTacToeGame () {
+
+    public TicTacToeGame() {
         initGameField();
     }
-    private void initGameField () {
+
+    private void initGameField() {
         // Init Game Field main window
         GAME_FIELD.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        GAME_FIELD.setSize(400,400);
-        GAME_FIELD.getContentPane().setBackground(new Color(153,255,153));
+        GAME_FIELD.setSize(400, 400);
+        GAME_FIELD.getContentPane().setBackground(new Color(153, 255, 153));
         GAME_FIELD.setLayout(new BorderLayout());
         GAME_FIELD.setResizable(false);
         GAME_FIELD.setVisible(true);
 
         // Init Text Field within Game Field window
-        TEXT_FIELD.setBackground(new Color (51,255,51));
-        TEXT_FIELD.setForeground(new Color(47,49,63));
+        TEXT_FIELD.setBackground(new Color(51, 255, 51));
+        TEXT_FIELD.setForeground(new Color(47, 49, 63));
         TEXT_FIELD.setFont(new Font("Roboto", BOLD, 40));
         TEXT_FIELD.setHorizontalAlignment(CENTER);
         TEXT_FIELD.setText("Tic Tac Toe");
@@ -42,19 +44,21 @@ public class TicTacToeGame implements ActionListener {
 
         // Init title component of Game Field
         TITLE_PANEL.setLayout(new BorderLayout());
-        TITLE_PANEL.setBounds(0,0,GAME_FIELD.getWidth(),100);
+        TITLE_PANEL.setBounds(0, 0, GAME_FIELD.getWidth(), 100);
 
         // Init button panel (container for buttons)
-        BUTTONS_PANEL.setLayout(new GridLayout(3,3));
-        BUTTONS_PANEL.setBackground(new Color(255,255,255));
+        BUTTONS_PANEL.setLayout(new GridLayout(3, 3));
+        BUTTONS_PANEL.setBackground(new Color(0, 0, 0));
 
-        for(int buttonNr = 0; buttonNr < 9; buttonNr++) {
-            initButtons
-                    .andThen(button -> button.addActionListener(this))
-                    .andThen(button -> button.setFocusable(false))
-                    .andThen(button -> button.setFont(new Font("MV Boli", BOLD, 40)))
-                    .accept(BUTTONS_ARRAY[buttonNr] = new JButton());
-        }
+        initButtons();
+        /*for(int buttonNr = 0; buttonNr < 9; buttonNr++) {
+            BUTTONS_ARRAY[buttonNr] = new JButton();
+            BUTTONS_PANEL.add(BUTTONS_ARRAY[buttonNr]);
+            BUTTONS_ARRAY[buttonNr].setFont(new Font("MV Boli", BOLD, 120));
+            BUTTONS_ARRAY[buttonNr].setFocusable(false);
+            BUTTONS_ARRAY[buttonNr].addActionListener(this);
+
+        }*/
 
         // Add TEXT field to TITLE panel
         TITLE_PANEL.add(TEXT_FIELD);
@@ -62,10 +66,36 @@ public class TicTacToeGame implements ActionListener {
         // Add TITLE panel to GAME FIELD and stick to the top of view
         GAME_FIELD.add(TITLE_PANEL, NORTH);
         GAME_FIELD.add(BUTTONS_PANEL);
-
+        whosTurn();
     }
+
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 
+    }
+
+    public void initButtons() {
+        for (int buttonNr = 0; buttonNr < 9; buttonNr++) {
+            initButtons
+                    .andThen(button -> button.addActionListener(this))
+                    .andThen(button -> button.setFocusable(false))
+                    .andThen(button -> button.setFont(new Font("MV Boli", BOLD, 40)))
+                    .accept(BUTTONS_ARRAY[buttonNr] = new JButton());
+        }
+    }
+
+    public void whosTurn() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException sysInterrupt) {
+            sysInterrupt.printStackTrace();
+        }
+        if (generator.nextInt(2) == 1) {
+            isPlayerOneTurn = true;
+            TEXT_FIELD.setText("X turn");
+        } else {
+            isPlayerOneTurn = false;
+            TEXT_FIELD.setText("O turn");
+        }
     }
 }

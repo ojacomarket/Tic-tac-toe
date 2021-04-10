60d2a946af348de805e94c8d3b469514ddb79e94
ojacomarket <elfutbolesmivida@mail.ru>
2021-04-10 11:52:45 +0300

    Add random number generator to decide whos turn at start

--- src/main/java/com/reji/tictactoe/tictactoe/TicTacToeGame.java
+++ src/main/java/com/reji/tictactoe/tictactoe/TicTacToeGame.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class TicTacToeGame implements ActionListener {
     private final JFrame GAME_FIELD = new JFrame();
@@ -12,7 +13,8 @@ public class TicTacToeGame implements ActionListener {
     private final JPanel BUTTONS_PANEL = new JPanel();
     private final JLabel TEXT_FIELD = new JLabel();
     private final List<JButton>  BUTTONS_ITSELF = new ArrayList<>();
-    private boolean isPlayerOneTurn = true;
+    private final Random generator = new Random();
+    private boolean isPlayerOneTurn;
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 
